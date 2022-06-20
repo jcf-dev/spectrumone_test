ad593043e1cbcdc6093dec4a6da110db3d41dd73
Joween Flores <4754752+joweenflores@users.noreply.github.com>
2022-06-20 14:58:54 +0800

    SOT-5, Email Confirmation

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 
 urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
+    path(
+        "auth/register/account-confirm-email/<str:key>/",
+        ConfirmEmailView.as_view(),
+    ),
     path("auth/register/", include("dj_rest_auth.registration.urls")),
+    path(
+        "auth/register/account-confirm-email/",
+        VerifyEmailView.as_view(),
+        name="account_email_verification_sent",
+    ),
 ]
